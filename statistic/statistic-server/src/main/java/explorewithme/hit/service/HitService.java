a0342aba849c61e5439c1152c090e0243ef0b5e6
Zel-ik <84323978+Zel-ik@users.noreply.github.com>
2023-08-02 07:39:13 +0300

    delete old directory

--- statistic/statistic-server/src/main/java/explorewithme/hit/service/HitService.java
+++
@@ -1,7 +0,0 @@
-package explorewithme.hit.service;
-
-import explorewithme.dto.HitDto;
-
-public interface HitService {
-    void create(HitDto hitDto);
-}
--- statistic/statistic-server/src/main/java/explorewithme/stat/service/StatService.java
+++
@@ -1,9 +0,0 @@
-package explorewithme.stat.service;
-
-import explorewithme.dto.StatDto;
-
-import java.util.List;
-
-public interface StatService {
-    List<StatDto> getStats(String start, String end, List<String> uris, boolean unique);
-}
